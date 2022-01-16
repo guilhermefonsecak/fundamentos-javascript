a504e8005d21cd0df093764a2a7da783cde468f2
guilhermefonsecak <guilhermefon025@gmail.com>
2022-01-16 15:10:19 -0300

    adicionado ola mundo e calculadora basica

---
+++ operacoes.js
@@ -0,0 +1,18 @@
+let soma=1+1
+console.log("a soma desses numeros é", soma)
+let divisao=10/2
+console.log("o resultado da divisao é", divisao)
+let multiplicacao=5*5
+console.log("o resultado da multiplicacao é", multiplicacao)
+let subtracao=10-5
+console.log("o resultado da subtracao é", subtracao)
+
+let nome="guilherme"
+let dia=25
+let mes=04
+let ano=1999
+
+
+console.log("meu nome é", nome)
+console.log("nascido em", dia +"/" + mes +"/" + ano)
+console.log("minha idade é", 2022-ano)
